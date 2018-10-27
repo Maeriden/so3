30accf17ebf9b5e3fc2dc13a65af617518f5b8af
Maeriden <maeriden@gmail.com>
2018-10-28 00:08:56 +0200

    Changed platform interface

--- code/platform.h
+++ code/platform.h
@@ -10,17 +10,201 @@ typedef uint8_t  u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
 typedef uint64_t u64;
-typedef u32  b32;
+typedef u32      b32;
 
 // NOTE: These types simply mean the string is expected to be null-terminated
 typedef char*       Str0;
 typedef const char* const_Str0;
 
-i32 platform_recv(int socket, u8** out_data, u32* out_data_size);
-i32 platform_send(int socket, u8* data, u32 data_size);
-i32 platform_check_authorization(Str0 auth_string, u32 auth_string_len, b32* out_authorized);
-i32 platform_put_resource(Str0 resource_path, u32 resource_path_len, const u8* content, u32 content_size);
-i32 platform_get_resource(Str0 resource_path, u32 resource_path_len, u8** out_content, u32* out_content_size);
-i32 platform_syslog(u8 address[4], const_Str0 userid, const_Str0 method, const_Str0 path, u32 minor, u32 status, u32 resource_size);
 
+
+
+#define until(expr) while(!(expr))
+#define ARRAY_COUNT(a) ( sizeof(a) / sizeof(*a) )
+#define MIN(a, b)      ( (a) < (b) ? (a) : (b) )
+
+
+
+
+#if PLATFORM_LINUX
+	typedef int             filedes_t;
+	typedef pid_t           process_t;
+	typedef pthread_t       thread_t;
+	typedef int             socket_t;
+	typedef pthread_mutex_t mutex_t;
+	typedef pthread_mutex_t critsec_t;
+	typedef pthread_cond_t  condvar_t;
+	
+	typedef void* thread_callback_t(void*);
+	
+	#define IS_VALID_FILEDES(val) ( (val) != -1 )
+	#define IS_VALID_PROCESS(val) // TODO
+	#define IS_VALID_THREAD(val)  // TODO
+	#define IS_VALID_SOCKET(val)  ( (val) != -1 )
+	#define IS_VALID_MUTEX(val)   ( (val) != NULL ) // TODO: Check if correct
+	#define IS_VALID_CRITSEC(val) // TODO
+	#define IS_VALID_CONDVAR(val) // TODO
+	
+	#define SOCKET_SHUTDOWN_R  SHUT_RD
+	#define SOCKET_SHUTDOWN_W  SHUT_WR
+	#define SOCKET_SHUTDOWN_RW SHUT_RDWR
+	
+	#define PRINT_ERROR_FL(F, L, ...) // TODO
+	#define PRINT_WARN_FL(F, L, ...)  // TODO
+	#define PRINT_DEBUG_FL(F, L, ...) // TODO
 #endif
+
+
+
+
+
+
+#if PLATFORM_WIN32
+	typedef HANDLE             filedes_t;
+	typedef HANDLE             process_t;
+	typedef HANDLE             thread_t;
+	typedef SOCKET             socket_t;
+	typedef HANDLE             mutex_t;
+	typedef CRITICAL_SECTION   critsec_t;
+	typedef CONDITION_VARIABLE condvar_t;
+	
+	typedef SECURITY_ATTRIBUTES mutex_attr_t;
+	typedef DWORD               critsec_attr_t;
+	typedef void                condvar_attr_t;
+	
+	typedef DWORD WINAPI thread_callback_t(LPVOID);
+	
+	#define IS_VALID_FILEDES(val) ( (val) != INVALID_HANDLE_VALUE )
+	#define IS_VALID_PROCESS(val) ( 1 )
+	#define IS_VALID_THREAD(val)  ( (val) != NULL )
+	#define IS_VALID_SOCKET(val)  ( (val) != INVALID_SOCKET )
+	#define IS_VALID_MUTEX(val)   ( (val) != NULL )
+	#define IS_VALID_CRITSEC(val) ( 1 )
+	#define IS_VALID_CONDVAR(val) ( 1 ) 
+	
+	#define SOCKET_SHUTDOWN_R  SD_RECEIVE
+	#define SOCKET_SHUTDOWN_W  SD_SEND
+	#define SOCKET_SHUTDOWN_RW SD_BOTH
+	
+	#define STDIN_FILENO GetStdHandle(0)
+	
+	#define PRINT_ERROR_FL(F, L, ...) platform_print(F, L, 1, "[EE] ", __VA_ARGS__)
+	#define PRINT_WARN_FL(F, L, ...)  platform_print(F, L, 2, "[WW] ", __VA_ARGS__)
+	#define PRINT_DEBUG_FL(F, L, ...) platform_print(F, L, 3, "[DD] ", __VA_ARGS__)
+	
+	typedef struct dirent
+	{
+		u32  d_attr;
+		char d_name[1];
+	} Dirent;
+	
+	typedef int scandir_select_t(const struct dirent*);
+	typedef int scandir_compar_t(const struct dirent**, const struct dirent**);
+#endif
+
+
+
+#define PRINT_ERROR(...) PRINT_ERROR_FL(__FILE__, __LINE__, __VA_ARGS__)
+#define PRINT_WARN(...)  PRINT_WARN_FL (__FILE__, __LINE__, __VA_ARGS__)
+#define PRINT_DEBUG(...) PRINT_DEBUG_FL(__FILE__, __LINE__, __VA_ARGS__)
+
+
+
+void* platform_memory_alloc   (size_t size);
+void  platform_memory_free    (void* addr, size_t size);
+void* platform_memory_realloc (void* old_mem, size_t old_size, size_t new_size);
+
+
+void platform_print (const_Str0 file, int line, u32 level, const_Str0 prefix, const_Str0 format, ...);
+i32 platform_syslog (u8 address[4], const_Str0 userid, const_Str0 method, const_Str0 path, u32 minor, u32 status, u32 resource_size);
+
+
+i32 platform_thread_init (thread_t* thread, thread_callback_t* callback, void* callback_arg);
+i32 platform_thread_join (thread_t* thread);
+
+
+i32 platform_recv (socket_t socket, u8* buffer, u32 buffer_size, u32* out_recv_count);
+i32 platform_send (socket_t socket, u8* buffer, u32 buffer_size, u32* out_sent_count);
+
+
+i32 platform_mutex_attr_init    (mutex_attr_t* attrs);
+i32 platform_mutex_attr_destroy (mutex_attr_t* attrs);
+i32 platform_mutex_init    (mutex_t* mutex, mutex_attr_t* attrs, const_Str0 name);
+i32 platform_mutex_acquire (mutex_t* mutex);
+i32 platform_mutex_release (mutex_t* mutex);
+i32 platform_mutex_destroy (mutex_t* mutex);
+
+
+i32 platform_critsec_init    (critsec_t* critsec, critsec_attr_t* attrs);
+i32 platform_critsec_enter   (critsec_t* critsec);
+i32 platform_critsec_leave   (critsec_t* critsec);
+i32 platform_critsec_destroy (critsec_t* critsec);
+
+
+i32 platform_condvar_init       (condvar_t* condvar, condvar_attr_t* attrs);
+i32 platform_condvar_wait       (condvar_t* condvar, critsec_t* critsec);
+i32 platform_condvar_notify_any (condvar_t* condvar);
+i32 platform_condvar_notify_all (condvar_t* condvar);
+i32 platform_condvar_destroy    (condvar_t* condvar);
+
+
+typedef struct State State;
+typedef enum HTTP_STATUS
+{
+	HTTP_STATUS_NONE                  = 0,
+	HTTP_STATUS_OK                    = 200,
+	HTTP_STATUS_CREATED               = 201,
+	HTTP_STATUS_BAD_REQUEST           = 400,
+	HTTP_STATUS_UNAUTHORIZED          = 401,
+	HTTP_STATUS_NOT_FOUND             = 404,
+	HTTP_STATUS_METHOD_NOT_ALLOWED    = 405,
+	HTTP_STATUS_INTERNAL_SERVER_ERROR = 500,
+	HTTP_STATUS_NOT_IMPLEMENTED       = 501,
+} HTTP_STATUS;
+
+HTTP_STATUS platform_put_resource (State* state, Str0 resource_path, u32 resource_path_len, const u8* content, u32 content_size);
+HTTP_STATUS platform_get_resource (State* state, Str0 resource_path, u32 resource_path_len, u8** out_content, u32* out_content_size);
+
+
+
+
+static
+void* _memory_alloc(size_t size, const char* __file__, int __line__)
+{
+	if(!size) return NULL;
+	void* mem = platform_memory_alloc(size);
+	if(mem == NULL)
+	{
+		PRINT_ERROR_FL(__file__, __line__, "memory allocation of size %zu failed", size);
+		return NULL;
+	}
+	return mem;
+}
+#define memory_alloc(T, count) _memory_alloc(sizeof(T)*(count), __FILE__, __LINE__)
+
+
+static
+void _memory_free(void* addr, size_t size, const char* __file__, int __line__)
+{
+	if(!(addr && size)) return;
+	platform_memory_free(addr, size);
+}
+#define memory_free(T, addr, count) _memory_free(addr, sizeof(T)*(count), __FILE__, __LINE__)
+#define str0_free(str, len)          memory_free(char, str, (len)+1)
+
+
+static
+void* _memory_realloc(void* old_mem, size_t old_size, size_t new_size, const char* __file__, int __line__)
+{
+	void* new_mem = _memory_alloc(new_size, __file__, __line__);
+	if(new_mem)
+	{
+		memcpy(new_mem, old_mem, MIN(old_size, new_size));
+		_memory_free(old_mem, old_size, __file__, __line__);
+	}
+	return new_mem;
+}
+#define memory_realloc(T, oldm, oldc, newc) _memory_realloc(oldm, sizeof(T)*(oldc), sizeof(T)*(newc), __FILE__, __LINE__)
+
+
+#endif // PLATFORM_H
--- code/server.c
+++ code/server.c
@@ -1,36 +1,30 @@
 #include "platform.h"
 #include "base64.c"
 
+#define HTTP_STATUS_IS_SUCCESS(status)      ( 200 <= (status) && (status) <= 299 )
+#define HTTP_STATUS_IS_CLIENT_ERROR(status) ( 400 <= (status) && (status) <= 499 )
+#define HTTP_STATUS_IS_SERVER_ERROR(status) ( 500 <= (status) && (status) <= 599 )
 
-enum HTTP_STATUS
-{
-	HTTP_STATUS_OK                    = 200,
-	HTTP_STATUS_CREATED               = 201,
-	HTTP_STATUS_BAD_REQUEST           = 400,
-	HTTP_STATUS_UNAUTHORIZED          = 401,
-	HTTP_STATUS_NOT_FOUND             = 404,
-	HTTP_STATUS_INTERNAL_SERVER_ERROR = 500,
-	HTTP_STATUS_NOT_IMPLEMENTED       = 501,
-};
 
+typedef struct phr_header PhrHeader;
 
 typedef struct HTTPRequest
 {
-	int               minor_version;
-	size_t            method_len;
-	Str0              method;
-	size_t            path_len;
-	Str0              path;
-	size_t            headers_count;
-	struct phr_header headers[16];
-	size_t            content_len;
-	const u8*         content;
+	int       minor_version;
+	size_t    method_len;
+	Str0      method;
+	size_t    path_len;
+	Str0      path;
+	size_t    headers_count;
+	PhrHeader headers[16];
+	size_t    content_len;
+	const u8* content;
 } HTTPRequest;
 
 
 
 static
-const char* http_reason(u32 status_code)
+const_Str0 http_reason(u32 status_code)
 {
 	switch(status_code) {
 		case 200: return "OK";
@@ -38,6 +32,7 @@ const char* http_reason(u32 status_code)
 		case 400: return "Bad Request";
 		case 401: return "Unauthorized";
 		case 404: return "Not Found";
+		case 405: return "Method Not Allowed";
 		case 500: return "Internal Server Error";
 		case 501: return "Not Implemented";
 	} return "";
@@ -45,35 +40,80 @@ const char* http_reason(u32 status_code)
 
 
 static
-u32 format_response(u8* buffer, size_t buffer_size, u32 status, u8* content, u32 content_size)
+PhrHeader* server_find_header(PhrHeader headers[], u32 headers_count, const_Str0 sought_name)
 {
-	static char* format = "\
+	for(u32 i = 0; i < headers_count; ++i)
+		if(strN_indexof0(headers[i].name, headers[i].name_len, sought_name) != headers[i].name_len)
+			return headers + i;
+	return NULL;
+}
+
+
+static
+Str0 server_extract_userid(HTTPRequest* request)
+{
+	Str0 result = NULL;
+	PhrHeader* auth_header = server_find_header(request->headers, request->headers_count, "Authorization");
+	if(auth_header)
+	{
+		u32 OFF = strlen("Basic ");
+		size_t auth_string_len = 0;
+		u8*    auth_string     = base64_decode(auth_header->value+OFF, auth_header->value_len-OFF, &auth_string_len);
+		if(auth_string)
+		{
+			u32 sep_i = strN_indexofN(auth_string, auth_string_len, ":", 1);
+			result = strN_dup0(auth_string, sep_i);
+			free(auth_string);
+		}
+	}
+	return result;
+}
+
+
+static
+u32 server_format_response(u8* buffer, size_t buffer_size, u32 status, u8* content, u32 content_len)
+{
+	static const_Str0 FORMAT_OK = "\
 HTTP/1.0 %u %s\r\n\
 Server: os3-1701014/1.0.0\r\n\
 Content-Type: text/plain\r\n\
 Content-Length: %u\r\n\
 \r\n\
 %.*s";
+	
+	static const_Str0 FORMAT_UNAUTHORIZED = "\
+HTTP/1.0 %u %s\r\n\
+Server: os3-1701014/1.0.0\r\n\
+Content-Type: text/plain\r\n\
+Content-Length: %u\r\n\
+WWW-Authenticate: Basic\r\n\
+\r\n\
+%.*s";
+	
+	const_Str0 format = FORMAT_OK;
+	if(status == HTTP_STATUS_UNAUTHORIZED)
+		format = FORMAT_UNAUTHORIZED;
+	
 	return snprintf(buffer, buffer_size, format,
 	                status, http_reason(status),
-	                content_size,
-	                content_size, content);
+	                content_len,
+	                content_len, content);
 }
 
 
 static
-i32 send_response(int socket, u32 http_status, u8* content, u32 content_size)
+i32 server_send_response(socket_t socket, u32 http_status, u8* content, u32 content_size)
 {
-	u32  outgoing_data_len = format_response(NULL, 0, http_status, content, content_size);
-	Str0 outgoing_data     = memory_alloc(char, outgoing_data_len+1);
-	if(!outgoing_data)
-	{
-		return -1;
-	}
-	format_response(outgoing_data, outgoing_data_len+1, http_status, content, content_size);
+	u32  buffer_len = server_format_response(NULL, 0, http_status, content, content_size);
+	Str0 buffer     = memory_alloc(char, buffer_len+1);
+	if(!buffer)
+		return HTTP_STATUS_INTERNAL_SERVER_ERROR;
+	server_format_response(buffer, buffer_len+1, http_status, content, content_size);
 	
-	i32 error = platform_send(socket, outgoing_data, outgoing_data_len);
-	memory_free(char, outgoing_data, outgoing_data_len+1);
+	u32 sent_count = 0;
+	i32 error = platform_send(socket, buffer, buffer_len, &sent_count);
+	ASSERT(sent_count == buffer_len);
+	memory_free(char, buffer, buffer_len+1);
 	if(error)
 	{
 		PRINT_ERROR("platform_send() failed");
@@ -82,131 +122,12 @@ i32 send_response(int socket, u32 http_status, u8* content, u32 content_size)
 }
 
 
-i32 _parse_request(u8* incoming_data, u32 incoming_data_size, HTTPRequest* request)
+static
+i32 server_encrypt_response(u8* response_content, u32 response_content_size, u32 encryption_key)
 {
-	request->headers_count = ARRAY_COUNT(request->headers);
+	if(response_content == NULL || response_content_size == 0 || encryption_key == 0)
+		return 0;
 	
-	int consumed_size = phr_parse_request(incoming_data, incoming_data_size,
-	                                      (const char**)&request->method, &request->method_len,
-	                                      (const char**)&request->path,   &request->path_len,
-	                                      &request->minor_version,
-	                                      request->headers, &request->headers_count,
-	                                      0);
-	if(consumed_size < 0)
-	{
-		// NOTE: Assume client made a valid request and we suck
-		PRINT_ERROR("phr_parse_request() failed");
-		return -1;
-	}
-	request->content_len = incoming_data_size - consumed_size;
-	request->content     = incoming_data      + consumed_size;
-	
-	// Null-terminate by replacing space with \0
-	request->method[request->method_len] = 0;
-	request->path[request->path_len]     = 0;
-	
-	return 0;
-}
-
-
-i32 process_client_request(int socket, u8* incoming_data, u32 incoming_data_size,
-                           u8** out_response_content, u32* out_response_content_size,
-                           Str0* out_userid, Str0* out_method, Str0* out_path,  
-                           u32* out_minor_version, u32* out_resource_size)
-{
-	HTTPRequest request = {};
-	if(_parse_request(incoming_data, incoming_data_size, &request) != 0)
-	{
-		PRINT_ERROR("phr_parse_request() failed");
-		return 500;
-	}
-	
-	*out_method = strN_dup0(request.method, request.method_len);
-	*out_path   = strN_dup0(request.path, request.path_len);
-	*out_minor_version = request.minor_version;
-	*out_resource_size = request.content_len;
-	
-	size_t auth_string_len = 0;
-	u8*    auth_string     = NULL;
-	for(int i = 0; i < request.headers_count; ++i)
-	{
-		struct phr_header* header = request.headers + i;
-		if(strncmp(header->name, "Authorization", header->name_len) == 0)
-		{
-			auth_string = base64_decode(header->value, header->value_len, &auth_string_len);
-			if(!auth_string)
-			{
-				PRINT_ERROR("base64_decode() failed");
-				return 500;
-			}
-			
-			u32 sep_i = 0;
-			while(sep_i < auth_string_len && auth_string[sep_i] != ':')
-				sep_i += 1;
-			*out_userid = strN_dup0(auth_string, sep_i);
-			break;
-		}
-	}
-	
-	b32 authorized = 0;
-	i32 error = platform_check_authorization(auth_string, auth_string_len, &authorized);
-	free(auth_string);
-	if(error)
-	{
-		PRINT_ERROR("platform_check_authorization() failed");
-		return 500;
-	}
-	
-	if(!authorized)
-		return 401;
-	
-	if(request.path_len == 0)
-		return 400;
-	
-	// Str0 request_path0 = strn_dup0(request.path, request.path_len);
-	Str0 request_path0 = request.path;
-	if(!request_path0)
-		return 500;
-	
-	i32 http_status = 501;
-	if(strN_beginswith0(request.method, request.method_len, "PUT"))
-	{
-		http_status = 201;
-		i32 error = platform_put_resource(request_path0, request.path_len,
-		                                  request.content, request.content_len);
-		if(error == RESULT_CLIENT_ERROR)
-		{
-			PRINT_ERROR("put_resource_content() failed");
-			http_status = 400;
-		} else
-		if(error == RESULT_SERVER_ERROR)
-		{
-			PRINT_ERROR("put_resource_content() failed");
-			http_status = 500;
-		}
-		
-	} else
-	if(strN_beginswith0(request.method, request.method_len, "GET"))
-	{
-		http_status = 200;
-		int error = platform_get_resource(request_path0, request.path_len,
-		                                  out_response_content, out_response_content_size);
-		*out_resource_size = *out_response_content_size;
-		if(error)
-		{
-			PRINT_ERROR("get_resource_content() failed");
-			http_status = 500;
-		}
-		ASSERT(out_response_content != NULL);
-	}
-	
-	// free(request_path0);
-	return http_status;
-}
-
-
-i32 encode_response_content(u8* response_content, u32 response_content_size, u32 encryption_key)
-{
 	// Round up to multiple of 4
 	u32  buffer_size = (response_content_size + 3) & ~3;
 	u32* buffer      = memory_alloc(u8, buffer_size);
@@ -223,58 +144,270 @@ i32 encode_response_content(u8* response_content, u32 response_content_size, u32
 }
 
 
-i32 client_procedure(int socket, u32 encryption_key, u8 address[4])
-{	
-	u32   response_status = 0;
-	u32   response_content_size = 0;
-	u8*   response_content      = NULL;
+static
+int server_parse_request(u8* buffer, u32 buffer_size, HTTPRequest* out_request)
+{
+	out_request->headers_count = ARRAY_COUNT(out_request->headers);
+	int consumed_size = phr_parse_request(buffer, buffer_size,
+	                                      &out_request->method, &out_request->method_len,
+	                                      &out_request->path,   &out_request->path_len,
+	                                      &out_request->minor_version,
+	                                      out_request->headers, &out_request->headers_count,
+	                                      0);
+	return consumed_size;
+}
+
+
+static
+b32 server_check_authorization(State* state, char* auth_string, u32 auth_string_len)
+{
+	if(state->config.disable_authorization)
+		return 1;
 	
-	u32 incoming_data_size = 0;
-	u8* incoming_data      = NULL;
-	i32 error = platform_recv(socket, &incoming_data, &incoming_data_size);
-	if(error == RESULT_SUCCESS)
+	if(auth_string && auth_string_len > 0)
 	{
-		ASSERT(incoming_data != NULL);
+		ASSERT(state->users != NULL || state->users_count == 0);
 		
-		Str0 userid = NULL;
-		Str0 method = NULL;
-		Str0 path   = NULL;
-		u32  minor_version = 0;
-		u32  resource_size = 0;
-		response_status = process_client_request(socket, incoming_data, incoming_data_size,
-		                                         &response_content, &response_content_size,
-		                                         &userid, &method, &path, 
-		                                         &minor_version, &resource_size);
-		
-		platform_syslog(address, userid, method, path, minor_version, response_status, resource_size);
-		memory_free(u8, incoming_data, incoming_data_size);
-		memory_free(char, userid, strlen(userid));
-		memory_free(char, method, strlen(method));
-		memory_free(char, path,   strlen(path));
+		for(u32 i = 0; i < state->users_count; ++i)
+			if(str0_indexofN(state->users[i], auth_string, auth_string_len) == 0)
+				return 1;
 	}
-	else if(error == RESULT_CLIENT_ERROR)
-	{
-		response_status = 400;
-	}
-	else
-	{
-		PRINT_ERROR("platform_recv() failed");
-		response_status = 500;
-	}
+	return 0;
+}
+
+
+static
+HTTP_STATUS server_handle_request(State* state, socket_t socket, HTTPRequest* request, u8** out_response, u32* out_response_size,
+	                              u32* out_syslog_resource_size)
+{
+	if(request->path_len < 1)
+		return HTTP_STATUS_BAD_REQUEST;
 	
-	if(response_content && encryption_key != 0)
+	ASSERT(request->method != NULL);
+	ASSERT(request->path != NULL);
+	
+	
+	size_t auth_string_len = 0;
+	u8*    auth_string     = NULL;
+	struct phr_header* auth_header = server_find_header(request->headers, request->headers_count, "Authorization");
+	if(auth_header)
 	{
-		if(encode_response_content(response_content, response_content_size, encryption_key) != 0)
+		u32 offset = strlen("Basic ");
+		auth_string = base64_decode(auth_header->value+offset, auth_header->value_len-offset, &auth_string_len);
+		if(!auth_string)
 		{
-			memory_free(u8, response_content, response_content_size);
-			response_content      = NULL;
-			response_content_size = 0;
-			response_status = 500;
+			PRINT_ERROR("base64_decode() failed");
+			return HTTP_STATUS_INTERNAL_SERVER_ERROR;
 		}
 	}
 	
-	send_response(socket, response_status, response_content, response_content_size);
-	memory_free(u8, response_content, response_content_size);
+	b32 authorized = server_check_authorization(state, auth_string, auth_string_len);
+	free(auth_string);
+	if(!authorized)
+		return HTTP_STATUS_UNAUTHORIZED;
 	
-	return 0;
+	
+	if(strN_beginswith0(request->method, request->method_len, "PUT"))
+	{
+		*out_syslog_resource_size = request->content_len;
+		
+		if(request->path[request->path_len-1] == '/')
+		{
+			// Forbid PUTing using a path that looks like a directory
+			return HTTP_STATUS_BAD_REQUEST;
+		}
+		
+		if(str0_beginswith0(request->path, "/commands"))
+		{
+			// Forbid PUTing to commands directory
+			return HTTP_STATUS_UNAUTHORIZED;
+		}
+		
+		HTTP_STATUS http_status = platform_put_resource(state, request->path, request->path_len, request->content, request->content_len);
+		if(HTTP_STATUS_IS_SERVER_ERROR(http_status))
+		{
+			PRINT_ERROR("put_resource_content() failed");
+		}
+		
+		return http_status;
+	}
+	
+	
+	if(strN_beginswith0(request->method, request->method_len, "GET"))
+	{
+		// Path must never have a trailing slash when it gets to the platform layer
+		if(request->path[request->path_len-1] == '/')
+		{
+			request->path[request->path_len-1] = 0;
+			request->path -= 1;
+		}
+		
+		HTTP_STATUS http_status = platform_get_resource(state, request->path, request->path_len, out_response, out_response_size);
+		if(HTTP_STATUS_IS_SERVER_ERROR(http_status))
+		{
+			PRINT_ERROR("get_resource_content() failed");
+		}
+		*out_syslog_resource_size = *out_response_size;
+		
+		return http_status;
+	}
+	
+	
+	return HTTP_STATUS_NOT_IMPLEMENTED;
+}
+
+
+static
+HTTP_STATUS server_recv_request(socket_t socket, u8** out_buffer, u32* out_buffer_size, HTTPRequest* out_request)
+{
+	*out_buffer      = NULL;
+	*out_buffer_size = 0;
+	
+	u32 buffer_size = 4096;
+	u8* buffer      = memory_alloc(u8, buffer_size);
+	if(!buffer)
+		return HTTP_STATUS_INTERNAL_SERVER_ERROR;
+	
+	b32 recv_remain_found = 0;
+	u32 recv_remain       = 0;
+	u32 recv_total        = 0;
+	until(recv_remain_found && recv_remain == 0)
+	{
+		u8* avail_buffer      = buffer      + recv_total;
+		u32 avail_buffer_size = buffer_size - recv_total;
+		
+		u32 recv_count = 0;
+		if(platform_recv(socket, avail_buffer, avail_buffer_size, &recv_count) != 0)
+		{
+			PRINT_ERROR("platform_recv() failed");
+			memory_free(u8, buffer, buffer_size);
+			return HTTP_STATUS_INTERNAL_SERVER_ERROR;
+		}
+		
+		if(recv_count == 0)
+		{
+			PRINT_WARN("platform_recv() read 0 bytes");
+			break;
+		}
+		recv_total += recv_count;
+		
+		if(recv_remain_found)
+		{
+			recv_remain -= recv_count;
+			if(recv_remain == 0)
+				break;
+		}
+		else
+		{
+			if(strN_indexofN(avail_buffer, recv_count, "\r\n\r\n", 4) != recv_count)
+			{
+				recv_remain_found = 1;
+				recv_remain       = 0;
+				
+				HTTPRequest request = {0};
+				int consumed_size = server_parse_request(buffer, recv_total, &request);
+				if(consumed_size < 1)
+				{
+					PRINT_DEBUG("server_parse_request() failed");
+					memory_free(u8, buffer, buffer_size);
+					return HTTP_STATUS_BAD_REQUEST;
+				}
+				
+				struct phr_header* clen_header = server_find_header(request.headers, request.headers_count, "Content-Length");
+				if(clen_header)
+				{
+					u8* request_content = buffer + consumed_size;
+					
+					u32 nontent_len = request_content - buffer;
+					u32 content_already_received_len = recv_total - nontent_len;
+					
+					u32 content_len = strtoul(clen_header->value, NULL, 10);
+					u32 content_not_yet_received_len = content_len - content_already_received_len;
+					
+					recv_remain       = content_not_yet_received_len;
+					
+					// TODO: Perform a final allocation now that the message size is known?
+				}
+				else
+				{
+					// NOTE: Seems like not sending a Content-Length in a GET is ok
+					//PRINT_DEBUG("server_find_header(%s) failed", "Content-Length");
+					//memory_free(u8, buffer, buffer_size);
+					//return HTTP_STATUS_BAD_REQUEST;
+				}
+			}
+		}
+		
+		
+		if(recv_total == buffer_size)
+		{
+			if(recv_remain > 0 || !recv_remain_found)
+			{
+				u32 new_buffer_size = buffer_size + 4096;
+				u8* new_buffer      = memory_realloc(u8, buffer, buffer_size, new_buffer_size);
+				if(!new_buffer)
+				{
+					memory_free(u8, buffer, buffer_size);
+					return HTTP_STATUS_INTERNAL_SERVER_ERROR;
+				}
+				buffer      = new_buffer;
+				buffer_size = new_buffer_size;
+			}
+		}
+	}
+	
+	int noncontent_len = server_parse_request(buffer, recv_total, out_request);
+	if(out_request->method) out_request->method[out_request->method_len] = 0;
+	if(out_request->path)   out_request->path[out_request->path_len]     = 0;
+	
+	struct phr_header* clen_header = server_find_header(out_request->headers, out_request->headers_count, "Content-Length");
+	out_request->content     = clen_header ? buffer + noncontent_len               : NULL;
+	out_request->content_len = clen_header ? strtoul(clen_header->value, NULL, 10) : 0;
+	
+	*out_buffer      = buffer;
+	*out_buffer_size = buffer_size;
+	return HTTP_STATUS_OK;
+}
+
+
+void server_serve_client(State* state, socket_t socket, u32 encryption_key, u8 address[4])
+{
+	u32 request_buffer_size = 0;
+	u8* request_buffer      = NULL;
+	
+	u32 response_buffer_size = 0;
+	u8* response_buffer      = NULL;
+	
+	HTTPRequest request = {0};
+	HTTP_STATUS http_status = server_recv_request(socket, &request_buffer, &request_buffer_size, &request);
+	
+	u32 syslog_resource_size = 0;
+	if(HTTP_STATUS_IS_SUCCESS(http_status))
+	{
+		http_status = server_handle_request(state, socket, &request, &response_buffer, &response_buffer_size, &syslog_resource_size);
+		if(HTTP_STATUS_IS_SUCCESS(http_status))
+		{
+			if(server_encrypt_response(response_buffer, response_buffer_size, encryption_key) != 0)
+			{
+				PRINT_ERROR("server_encrypt_response() failed");
+				http_status = HTTP_STATUS_INTERNAL_SERVER_ERROR;
+			}
+		}
+	}
+	
+	if(server_send_response(socket, http_status, response_buffer, response_buffer_size) == 0)
+	{
+		Str0 userid = server_extract_userid(&request);
+		if(platform_syslog(address, userid, request.method, request.path, request.minor_version, http_status, syslog_resource_size) != 0)
+			PRINT_ERROR("platform_syslog() failed");
+		if(userid)
+			memory_free(char, userid, strlen(userid));
+	}
+	else
+	{
+		PRINT_ERROR("server_send_response() failed");
+	}
+	
+	memory_free(u8, request_buffer,  request_buffer_size);
+	memory_free(u8, response_buffer, response_buffer_size);
 }
