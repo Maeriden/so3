dca63fe64aff4b1e7f1b9741d0ac4d10a5a9e090
Maeriden <maeriden@gmail.com>
2018-10-29 23:44:15 +0100

    Changed platform interface

--- code/platform.h
+++ code/platform.h
@@ -35,26 +35,22 @@ typedef const char* const_Str0;
 	typedef pthread_mutex_t critsec_t;
 	typedef pthread_cond_t  condvar_t;
 	
-	typedef void* thread_callback_t(void*);
+	typedef pthread_mutexattr_t mutex_attr_t;
+	typedef pthread_mutexattr_t critsec_attr_t;
+	typedef pthread_condattr_t  condvar_attr_t;
 	
-	#define IS_VALID_FILEDES(val) ( (val) != -1 )
-	#define IS_VALID_PROCESS(val) // TODO
-	#define IS_VALID_THREAD(val)  // TODO
-	#define IS_VALID_SOCKET(val)  ( (val) != -1 )
-	#define IS_VALID_MUTEX(val)   ( (val) != NULL ) // TODO: Check if correct
-	#define IS_VALID_CRITSEC(val) // TODO
-	#define IS_VALID_CONDVAR(val) // TODO
+	#define THREAD_CALLBACK_SIGNATURE(name, param_name) void* name(void* param_name)
+	#define THREAD_CALLBACK_RETURN                      return NULL
+	typedef void* thread_callback_t(void*);
 	
 	#define SOCKET_SHUTDOWN_R  SHUT_RD
 	#define SOCKET_SHUTDOWN_W  SHUT_WR
 	#define SOCKET_SHUTDOWN_RW SHUT_RDWR
 	
-	#define PRINT_ERROR_FL(F, L, ...) // TODO
-	#define PRINT_WARN_FL(F, L, ...)  // TODO
-	#define PRINT_DEBUG_FL(F, L, ...) // TODO
+	#define PRINT_ERROR_FL(F, L, ...) platform_print(F, L, 1, "\e[31m[EE] ", __VA_ARGS__)
+	#define PRINT_WARN_FL(F, L, ...)  platform_print(F, L, 2, "\e[33m[WW] ", __VA_ARGS__)
+	#define PRINT_DEBUG_FL(F, L, ...) platform_print(F, L, 3, "\e[32m[DD] ", __VA_ARGS__)
 #endif
-
-
 
 
 
@@ -72,25 +68,21 @@ typedef const char* const_Str0;
 	typedef DWORD               critsec_attr_t;
 	typedef void                condvar_attr_t;
 	
+	#define THREAD_CALLBACK_SIGNATURE(name, param_name) DWORD WINAPI name(LPVOID param_name)
+	#define THREAD_CALLBACK_RETURN                      return 0
 	typedef DWORD WINAPI thread_callback_t(LPVOID);
-	
-	#define IS_VALID_FILEDES(val) ( (val) != INVALID_HANDLE_VALUE )
-	#define IS_VALID_PROCESS(val) ( 1 )
-	#define IS_VALID_THREAD(val)  ( (val) != NULL )
-	#define IS_VALID_SOCKET(val)  ( (val) != INVALID_SOCKET )
-	#define IS_VALID_MUTEX(val)   ( (val) != NULL )
-	#define IS_VALID_CRITSEC(val) ( 1 )
-	#define IS_VALID_CONDVAR(val) ( 1 ) 
 	
 	#define SOCKET_SHUTDOWN_R  SD_RECEIVE
 	#define SOCKET_SHUTDOWN_W  SD_SEND
 	#define SOCKET_SHUTDOWN_RW SD_BOTH
 	
-	#define STDIN_FILENO GetStdHandle(0)
-	
 	#define PRINT_ERROR_FL(F, L, ...) platform_print(F, L, 1, "[EE] ", __VA_ARGS__)
 	#define PRINT_WARN_FL(F, L, ...)  platform_print(F, L, 2, "[WW] ", __VA_ARGS__)
 	#define PRINT_DEBUG_FL(F, L, ...) platform_print(F, L, 3, "[DD] ", __VA_ARGS__)
+	
+	#define STDIN_FILENO  GetStdHandle(0)
+	#define STDOUT_FILENO GetStdHandle(1)
+	#define STDERR_FILENO GetStdHandle(2)
 	
 	typedef struct dirent
 	{
@@ -104,19 +96,27 @@ typedef const char* const_Str0;
 
 
 
+
+#if ENABLE_ASSERT
+	#include <assert.h>
+	#define ASSERT(c) assert(c)
+#else
+	#define ASSERT(c) do{ (void)sizeof(c); }while(0)
+#endif
+
 #define PRINT_ERROR(...) PRINT_ERROR_FL(__FILE__, __LINE__, __VA_ARGS__)
 #define PRINT_WARN(...)  PRINT_WARN_FL (__FILE__, __LINE__, __VA_ARGS__)
 #define PRINT_DEBUG(...) PRINT_DEBUG_FL(__FILE__, __LINE__, __VA_ARGS__)
 
 
 
-void* platform_memory_alloc   (size_t size);
-void  platform_memory_free    (void* addr, size_t size);
-void* platform_memory_realloc (void* old_mem, size_t old_size, size_t new_size);
+
+void* platform_memory_alloc (size_t size);
+i32   platform_memory_free  (void* addr, size_t size);
 
 
-void platform_print (const_Str0 file, int line, u32 level, const_Str0 prefix, const_Str0 format, ...);
-i32 platform_syslog (u8 address[4], const_Str0 userid, const_Str0 method, const_Str0 path, u32 minor, u32 status, u32 resource_size);
+void platform_print  (const_Str0 file, int line, u32 level, const_Str0 prefix, const_Str0 format, ...);
+i32  platform_syslog (u8 address[4], const_Str0 userid, const_Str0 method, const_Str0 path, u32 minor, u32 status, u32 resource_size);
 
 
 i32 platform_thread_init (thread_t* thread, thread_callback_t* callback, void* callback_arg);
@@ -127,21 +127,13 @@ i32 platform_recv (socket_t socket, u8* buffer, u32 buffer_size, u32* out_recv_c
 i32 platform_send (socket_t socket, u8* buffer, u32 buffer_size, u32* out_sent_count);
 
 
-i32 platform_mutex_attr_init    (mutex_attr_t* attrs);
-i32 platform_mutex_attr_destroy (mutex_attr_t* attrs);
-i32 platform_mutex_init    (mutex_t* mutex, mutex_attr_t* attrs, const_Str0 name);
-i32 platform_mutex_acquire (mutex_t* mutex);
-i32 platform_mutex_release (mutex_t* mutex);
-i32 platform_mutex_destroy (mutex_t* mutex);
-
-
-i32 platform_critsec_init    (critsec_t* critsec, critsec_attr_t* attrs);
+i32 platform_critsec_init    (critsec_t* critsec);
 i32 platform_critsec_enter   (critsec_t* critsec);
 i32 platform_critsec_leave   (critsec_t* critsec);
 i32 platform_critsec_destroy (critsec_t* critsec);
 
 
-i32 platform_condvar_init       (condvar_t* condvar, condvar_attr_t* attrs);
+i32 platform_condvar_init       (condvar_t* condvar);
 i32 platform_condvar_wait       (condvar_t* condvar, critsec_t* critsec);
 i32 platform_condvar_notify_any (condvar_t* condvar);
 i32 platform_condvar_notify_all (condvar_t* condvar);
@@ -151,13 +143,15 @@ i32 platform_condvar_destroy    (condvar_t* condvar);
 typedef struct State State;
 typedef enum HTTP_STATUS
 {
-	HTTP_STATUS_NONE                  = 0,
+	HTTP_STATUS_CONTINUE              = 100,
 	HTTP_STATUS_OK                    = 200,
 	HTTP_STATUS_CREATED               = 201,
 	HTTP_STATUS_BAD_REQUEST           = 400,
 	HTTP_STATUS_UNAUTHORIZED          = 401,
+	HTTP_STATUS_FORBIDDEN             = 403,
 	HTTP_STATUS_NOT_FOUND             = 404,
 	HTTP_STATUS_METHOD_NOT_ALLOWED    = 405,
+	HTTP_STATUS_EXPECTATION_FAILED    = 417,
 	HTTP_STATUS_INTERNAL_SERVER_ERROR = 500,
 	HTTP_STATUS_NOT_IMPLEMENTED       = 501,
 } HTTP_STATUS;
@@ -187,7 +181,10 @@ static
 void _memory_free(void* addr, size_t size, const char* __file__, int __line__)
 {
 	if(!(addr && size)) return;
-	platform_memory_free(addr, size);
+	if(platform_memory_free(addr, size) != 0)
+	{
+		PRINT_ERROR_FL(__file__, __line__, "memory deallocation of size %zu failed", size);
+	}
 }
 #define memory_free(T, addr, count) _memory_free(addr, sizeof(T)*(count), __FILE__, __LINE__)
 #define str0_free(str, len)          memory_free(char, str, (len)+1)
@@ -205,6 +202,5 @@ void* _memory_realloc(void* old_mem, size_t old_size, size_t new_size, const cha
 	return new_mem;
 }
 #define memory_realloc(T, oldm, oldc, newc) _memory_realloc(oldm, sizeof(T)*(oldc), sizeof(T)*(newc), __FILE__, __LINE__)
-
 
 #endif // PLATFORM_H
