0786b156af05310239686c271a2c637db61f369f
Maeriden <maeriden@gmail.com>
2018-11-12 03:32:24 +0100

    Changed request parsing logic

--- code/server.c
+++ code/server.c
@@ -17,8 +17,9 @@ typedef struct HTTPRequest
 	Str0      path;
 	size_t    headers_count;
 	PhrHeader headers[16];
+	size_t    content_offset;
 	size_t    content_len;
-	const u8* content;
+	u8*       content;
 } HTTPRequest;
 
 
@@ -115,7 +116,6 @@ i32 server_send_response(socket_t socket, u32 http_status, u8* content, u32 cont
 	
 	u32 sent_count = 0;
 	i32 error = platform_send(socket, buffer, buffer_len, &sent_count);
-	ASSERT(sent_count == buffer_len);
 	memory_free(char, buffer, buffer_len+1);
 	if(error)
 	{
@@ -224,6 +224,7 @@ HTTP_STATUS server_handle_request(State* state, socket_t socket, HTTPRequest* re
 	}
 	
 	
+	
 	if(strN_beginswith0(request->method, request->method_len, "PUT"))
 	{
 		*out_syslog_resource_size = request->content_len;
@@ -299,126 +300,294 @@ HTTP_STATUS server_handle_request(State* state, socket_t socket, HTTPRequest* re
 
 
 static
-HTTP_STATUS server_recv_request(socket_t socket, u8** out_buffer, u32* out_buffer_size, HTTPRequest* out_request)
+u32 server_recv_request_content(socket_t socket, u8* buffer, u32 buffer_size, u32 content_len, HTTP_STATUS* out_status)
+{
+	ASSERT(buffer_size >= content_len);
+	
+	*out_status = HTTP_STATUS_OK;
+	u32 recv_total = 0;
+	while(recv_total < content_len)
+	{
+		u32 avail_buffer_size = buffer_size - recv_total;
+		u8* avail_buffer      = buffer      + recv_total;
+		
+		u32 recv_count = 0;
+		i32 error = platform_recv(socket, avail_buffer, avail_buffer_size, &recv_count);
+		if(error)
+		{
+			PRINT_ERROR("platform_recv() failed");
+			*out_status = HTTP_STATUS_INTERNAL_SERVER_ERROR;
+			break;
+		}
+		
+		// NOTE: In the case of a ECONNRESET, we consider it a bad request since the content data
+		// is not the same as the declared Content-Length
+		if(recv_count == 0)
+		{
+			*out_status = HTTP_STATUS_BAD_REQUEST;
+			break;
+		}
+		
+		recv_total += recv_count;
+	}
+	return recv_total;
+}
+
+
+static
+u32 server_recv_request(socket_t socket, u8** out_buffer, u32* out_buffer_size, HTTPRequest* out_request, HTTP_STATUS* out_status)
 {
 	*out_buffer      = NULL;
 	*out_buffer_size = 0;
+	*out_status      = HTTP_STATUS_OK;
 	
 	u32 buffer_size = 4096;
 	u8* buffer      = memory_alloc(u8, buffer_size);
 	if(!buffer)
-		return HTTP_STATUS_INTERNAL_SERVER_ERROR;
-	
-	HTTP_STATUS http_status = 200;
-	
-	b32 recv_remain_found = 0;
-	u32 recv_remain       = 0;
-	u32 recv_total        = 0;
-	until(recv_remain_found && recv_remain == 0)
 	{
+		*out_status = HTTP_STATUS_INTERNAL_SERVER_ERROR;
+		return 0;
+	}
+	
+	u32 recv_total = 0;
+	b32 headers_found = 0;
+	until(headers_found)
+	{
+		if(recv_total == buffer_size)
+		{
+			u32 new_buffer_size = buffer_size + 4096;
+			u8* new_buffer      = memory_realloc(u8, buffer, buffer_size, new_buffer_size);
+			if(!new_buffer)
+			{
+				memory_free(u8, buffer, buffer_size);
+				*out_status = HTTP_STATUS_INTERNAL_SERVER_ERROR;
+				return recv_total;
+			}
+			buffer      = new_buffer;
+			buffer_size = new_buffer_size;
+		}
+		
 		u8* avail_buffer      = buffer      + recv_total;
 		u32 avail_buffer_size = buffer_size - recv_total;
 		
 		u32 recv_count = 0;
-		if(platform_recv(socket, avail_buffer, avail_buffer_size, &recv_count) != 0)
+		i32 error = platform_recv(socket, avail_buffer, avail_buffer_size, &recv_count);
+		if(error)
 		{
 			PRINT_ERROR("platform_recv() failed");
 			memory_free(u8, buffer, buffer_size);
-			return HTTP_STATUS_INTERNAL_SERVER_ERROR;
+			*out_status = HTTP_STATUS_INTERNAL_SERVER_ERROR;
+			return recv_total;
 		}
 		
+		// NOTE: If client closes connection early, recv will signal ECONNRESET and recv_count will be 0
 		if(recv_count == 0)
 		{
-			PRINT_WARN("platform_recv() read 0 bytes");
-			break;
+			memory_free(u8, buffer, buffer_size);
+			*out_status = HTTP_STATUS_BAD_REQUEST;
+			return recv_total;
 		}
-		recv_total += recv_count;
 		
-		if(recv_remain_found)
+		recv_total += recv_count;
+		headers_found = strN_containsN(avail_buffer, recv_count, "\r\n\r\n", 4);
+	}
+	
+	
+	HTTPRequest request = {0};
+	int consumed_size = server_parse_request(buffer, recv_total, &request);
+	if(consumed_size < 1)
+	{
+		PRINT_ERROR("server_parse_request() failed");
+		memory_free(u8, buffer, buffer_size);
+		*out_status = HTTP_STATUS_BAD_REQUEST;
+		return recv_total;
+	}
+	request.content_offset = consumed_size;
+	request.content = buffer + request.content_offset;
+	
+	ASSERT(request.method);
+	ASSERT(request.path);
+	ASSERT(request.method_len > 0);
+	ASSERT(request.path_len > 0);
+	
+	struct phr_header* clen_header = server_find_header(request.headers, request.headers_count, "Content-Length");
+	if(clen_header)
+	{
+		request.content_len = strtoul(clen_header->value, NULL, 10);
+	}
+	
+	// Resize buffer to the exact size we need for the request
+	u32 final_buffer_size = request.content_offset + request.content_len;
+	if(final_buffer_size != buffer_size)
+	{
+		u8* final_buffer = memory_realloc(u8, buffer, buffer_size, final_buffer_size);
+		if(final_buffer)
 		{
-			recv_remain -= recv_count;
-			if(recv_remain == 0)
-				break;
+			buffer      = final_buffer;
+			buffer_size = final_buffer_size;
+			if(server_parse_request(buffer, recv_total, &request) == consumed_size)
+			{
+				request.content = buffer + request.content_offset;
+			}
+			else
+			{
+				PRINT_ERROR("server_parse_request() failed");
+				*out_status = HTTP_STATUS_INTERNAL_SERVER_ERROR;
+			}
 		}
 		else
 		{
-			if(strN_indexofN(avail_buffer, recv_count, "\r\n\r\n", 4) != recv_count)
-			{
-				recv_remain_found = 1;
-				recv_remain       = 0;
-				
-				HTTPRequest request = {0};
-				int consumed_size = server_parse_request(buffer, recv_total, &request);
-				if(consumed_size < 1)
-				{
-					// PRINT_DEBUG("server_parse_request() failed");
-					memory_free(u8, buffer, buffer_size);
-					return HTTP_STATUS_BAD_REQUEST;
-				}
-				
-				struct phr_header* xpct_header = server_find_header(request.headers, request.headers_count, "Expect");
-				if(xpct_header)
-				{
-					http_status = HTTP_STATUS_EXPECTATION_FAILED;
-					break;
-				}
-				
-				struct phr_header* clen_header = server_find_header(request.headers, request.headers_count, "Content-Length");
-				if(clen_header)
-				{
-					u8* request_content = buffer + consumed_size;
-					
-					u32 nontent_len = request_content - buffer;
-					u32 content_already_received_len = recv_total - nontent_len;
-					
-					u32 content_len = strtoul(clen_header->value, NULL, 10);
-					u32 content_not_yet_received_len = content_len - content_already_received_len;
-					
-					recv_remain = content_not_yet_received_len;
-					
-					// TODO: Perform a final allocation now that the message size is known?
-				}
-			}
-		}
-		
-		
-		if(recv_total == buffer_size)
-		{
-			if(recv_remain > 0 || !recv_remain_found)
-			{
-				u32 new_buffer_size = buffer_size + 4096;
-				u8* new_buffer      = memory_realloc(u8, buffer, buffer_size, new_buffer_size);
-				if(!new_buffer)
-				{
-					memory_free(u8, buffer, buffer_size);
-					return HTTP_STATUS_INTERNAL_SERVER_ERROR;
-				}
-				buffer      = new_buffer;
-				buffer_size = new_buffer_size;
-			}
+			*out_status = HTTP_STATUS_INTERNAL_SERVER_ERROR;
 		}
 	}
 	
-	HTTPRequest request = {0};
-	int noncontent_len = server_parse_request(buffer, recv_total, &request);
-	if(noncontent_len < 1)
-	{
-		memory_free(u8, buffer, buffer_size);
-		return HTTP_STATUS_BAD_REQUEST;
-	}
-	
-	*out_request = request;
-	if(out_request->method) out_request->method[out_request->method_len] = 0;
-	if(out_request->path)   out_request->path[out_request->path_len]     = 0;
-	
-	struct phr_header* clen_header = server_find_header(out_request->headers, out_request->headers_count, "Content-Length");
-	out_request->content     = clen_header ? buffer + noncontent_len               : NULL;
-	out_request->content_len = clen_header ? strtoul(clen_header->value, NULL, 10) : 0;
+	request.method[request.method_len] = 0;
+	request.path[request.path_len]     = 0;
 	
 	*out_buffer      = buffer;
 	*out_buffer_size = buffer_size;
-	return http_status;
+	*out_request     = request;
+	return recv_total;
 }
+
+
+// static
+// HTTP_STATUS server_recv_request(socket_t socket, u8** out_buffer, u32* out_buffer_size, HTTPRequest* out_request)
+// {
+// 	*out_buffer      = NULL;
+// 	*out_buffer_size = 0;
+	
+// 	u32 buffer_size = 4096;
+// 	u8* buffer      = memory_alloc(u8, buffer_size);
+// 	if(!buffer)
+// 		return HTTP_STATUS_INTERNAL_SERVER_ERROR;
+	
+// 	HTTP_STATUS http_status = 200;
+	
+// 	b32 recv_remain_found = 0;
+// 	u32 recv_remain       = 0;
+// 	u32 recv_total        = 0;
+// 	until(recv_remain_found && recv_remain == 0)
+// 	{
+// 		u8* avail_buffer      = buffer      + recv_total;
+// 		u32 avail_buffer_size = buffer_size - recv_total;
+		
+// 		u32 recv_count = 0;
+// 		i32 error = platform_recv(socket, avail_buffer, avail_buffer_size, &recv_count);
+// 		if(error)
+// 		{
+// 			PRINT_ERROR("platform_recv() failed");
+// 			memory_free(u8, buffer, buffer_size);
+// 			return HTTP_STATUS_INTERNAL_SERVER_ERROR;
+// 		}
+		
+// 		if(recv_count == 0)
+// 		{
+// 			// PRINT_ERROR("platform_recv() read 0 bytes");
+// 			memory_free(u8, buffer, buffer_size);
+// 			return HTTP_STATUS_BAD_REQUEST;
+// 		}
+		
+// 		recv_total += recv_count;
+		
+// 		if(recv_remain_found)
+// 		{
+// 			recv_remain -= recv_count;
+// 			if(recv_remain == 0)
+// 				break;
+// 		}
+// 		else
+// 		{
+// 			if(strN_indexofN(avail_buffer, recv_count, "\r\n\r\n", 4) != recv_count)
+// 			{
+// 				recv_remain_found = 1;
+// 				recv_remain       = 0;
+				
+// 				HTTPRequest request = {0};
+// 				int consumed_size = server_parse_request(buffer, recv_total, &request);
+// 				if(consumed_size < 1)
+// 				{
+// 					// PRINT_DEBUG("server_parse_request() failed");
+// 					memory_free(u8, buffer, buffer_size);
+// 					return HTTP_STATUS_BAD_REQUEST;
+// 				}
+				
+// 				struct phr_header* xpct_header = server_find_header(request.headers, request.headers_count, "Expect");
+// 				if(xpct_header)
+// 				{
+// 					http_status = HTTP_STATUS_EXPECTATION_FAILED;
+// 					break;
+// 				}
+				
+// 				struct phr_header* clen_header = server_find_header(request.headers, request.headers_count, "Content-Length");
+// 				if(clen_header)
+// 				{
+// 					u8* request_content = buffer + consumed_size;
+					
+// 					u32 nontent_len = request_content - buffer;
+// 					u32 content_already_received_len = recv_total - nontent_len;
+					
+// 					u32 content_len = strtoul(clen_header->value, NULL, 10);
+// 					u32 content_not_yet_received_len = content_len - content_already_received_len;
+					
+// 					recv_remain = content_not_yet_received_len;
+					
+// 					// Perform a final allocation now that the message size is known
+// 					u32 total_size = nontent_len + content_len;
+// 					if(total_size > buffer_size)
+// 					{
+// 						u32 new_buffer_size = total_size;
+// 						u8* new_buffer      = memory_realloc(u8, buffer, buffer_size, new_buffer_size);
+// 						if(!new_buffer)
+// 						{
+// 							memory_free(u8, buffer, buffer_size);
+// 							return HTTP_STATUS_INTERNAL_SERVER_ERROR;
+// 						}
+// 						buffer      = new_buffer;
+// 						buffer_size = new_buffer_size;
+// 					}
+// 				}
+// 			}
+// 		}
+		
+		
+// 		if(recv_total == buffer_size)
+// 		{
+// 			if(recv_remain > 0 || !recv_remain_found)
+// 			{
+// 				u32 new_buffer_size = buffer_size + 4096;
+// 				u8* new_buffer      = memory_realloc(u8, buffer, buffer_size, new_buffer_size);
+// 				if(!new_buffer)
+// 				{
+// 					memory_free(u8, buffer, buffer_size);
+// 					return HTTP_STATUS_INTERNAL_SERVER_ERROR;
+// 				}
+// 				buffer      = new_buffer;
+// 				buffer_size = new_buffer_size;
+// 			}
+// 		}
+// 	}
+	
+// 	HTTPRequest request = {0};
+// 	int noncontent_len = server_parse_request(buffer, recv_total, &request);
+// 	if(noncontent_len < 1)
+// 	{
+// 		memory_free(u8, buffer, buffer_size);
+// 		return HTTP_STATUS_BAD_REQUEST;
+// 	}
+	
+// 	*out_request = request;
+// 	if(out_request->method) out_request->method[out_request->method_len] = 0;
+// 	if(out_request->path)   out_request->path[out_request->path_len]     = 0;
+	
+// 	struct phr_header* clen_header = server_find_header(out_request->headers, out_request->headers_count, "Content-Length");
+// 	out_request->content     = clen_header ? buffer + noncontent_len               : NULL;
+// 	out_request->content_len = clen_header ? strtoul(clen_header->value, NULL, 10) : 0;
+	
+// 	*out_buffer      = buffer;
+// 	*out_buffer_size = buffer_size;
+// 	return http_status;
+// }
 
 
 void server_serve_client(State* state, socket_t socket, u32 encryption_key, ipv4_addr_t address)
@@ -429,37 +598,77 @@ void server_serve_client(State* state, socket_t socket, u32 encryption_key, ipv4
 	u32 response_buffer_size = 0;
 	u8* response_buffer      = NULL;
 	
-	HTTPRequest request = {0};
-	HTTP_STATUS http_status = server_recv_request(socket, &request_buffer, &request_buffer_size, &request);
+	// HTTPRequest request = {0};
+	// HTTP_STATUS http_status = server_recv_request(socket, &request_buffer, &request_buffer_size, &request);
 	
-	b32 do_syslog = 1;
+	// b32 do_syslog = 1;
+	// u32 syslog_resource_size = 0;
+	// if(HTTP_STATUS_IS_SUCCESS(http_status))
+	// {
+	// 	http_status = server_handle_request(state, socket, &request, &response_buffer, &response_buffer_size, &syslog_resource_size);
+	// 	if(HTTP_STATUS_IS_SUCCESS(http_status) && encryption_key != 0)
+	// 	{
+	// 		if(server_encrypt_response(response_buffer, response_buffer_size, encryption_key) != 0)
+	// 		{
+	// 			PRINT_ERROR("server_encrypt_response() failed");
+	// 			http_status = HTTP_STATUS_INTERNAL_SERVER_ERROR;
+	// 		}
+	// 	}
+	// }
+	
+	
 	u32 syslog_resource_size = 0;
+	HTTPRequest request     = {0};
+	HTTP_STATUS http_status = HTTP_STATUS_OK;
+	
+	u32 recv_count = server_recv_request(socket, &request_buffer, &request_buffer_size, &request, &http_status);
 	if(HTTP_STATUS_IS_SUCCESS(http_status))
 	{
-		http_status = server_handle_request(state, socket, &request, &response_buffer, &response_buffer_size, &syslog_resource_size);
-		if(HTTP_STATUS_IS_SUCCESS(http_status) && encryption_key != 0)
+		ASSERT(recv_count > 0);
+		
+		u32 content_buffer_len = request_buffer_size - recv_count;
+		u8* content_buffer     = request_buffer      + recv_count;
+		u32 remaining_content_len = request.content_offset + request.content_len - recv_count;
+		
+		recv_count += server_recv_request_content(socket, content_buffer, content_buffer_len, remaining_content_len, &http_status);
+		if(HTTP_STATUS_IS_SUCCESS(http_status))
 		{
-			if(server_encrypt_response(response_buffer, response_buffer_size, encryption_key) != 0)
+			ASSERT(recv_count == request.content_offset + request.content_len);
+			
+			http_status = server_handle_request(state, socket, &request, &response_buffer, &response_buffer_size, &syslog_resource_size);
+			if(HTTP_STATUS_IS_SUCCESS(http_status) && encryption_key != 0)
 			{
-				PRINT_ERROR("server_encrypt_response() failed");
-				http_status = HTTP_STATUS_INTERNAL_SERVER_ERROR;
+				if(server_encrypt_response(response_buffer, response_buffer_size, encryption_key) != 0)
+				{
+					PRINT_ERROR("server_encrypt_response() failed");
+					http_status = HTTP_STATUS_INTERNAL_SERVER_ERROR;
+				}
+			}
+			else if(HTTP_STATUS_IS_SERVER_ERROR(http_status))
+			{
+				PRINT_ERROR("server_handle_request() failed");
 			}
 		}
+		else if(HTTP_STATUS_IS_SERVER_ERROR(http_status))
+		{
+			PRINT_ERROR("server_recv_request_content() failed");
+		}
+	}
+	else if(HTTP_STATUS_IS_SERVER_ERROR(http_status))
+	{
+		PRINT_ERROR("server_recv_request() failed");
 	}
 	
 	
 	if(server_send_response(socket, http_status, response_buffer, response_buffer_size) == 0)
 	{
-		if(do_syslog)
+		Str0 userid = server_extract_userid(&request);
+		if(platform_syslog(address, userid, request.method, request.path, request.minor_version, http_status, syslog_resource_size) != 0)
 		{
-			Str0 userid = server_extract_userid(&request);
-			if(platform_syslog(address, userid, request.method, request.path, request.minor_version, http_status, syslog_resource_size) != 0)
-			{
-				PRINT_ERROR("platform_syslog() failed");
-			}
-			if(userid)
-				memory_free(char, userid, strlen(userid));
+			PRINT_ERROR("platform_syslog() failed");
 		}
+		if(userid)
+			memory_free(char, userid, strlen(userid));
 	}
 	else
 	{
